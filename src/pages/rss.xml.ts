929e65ebdb6ba48ae63098d5e5b0ba91e2d49535
Mateusz Ż <zochowski.mateusz@outlook.com>
2023-11-06 20:02:24 +0100

    add cache headers for /rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,22 @@
-import rss from '@astrojs/rss';
+import { getRssString } from '@astrojs/rss';
 import type { APIContext } from 'astro';
-
 
 export async function GET({ request }: APIContext) {
   const { origin } = new URL(request.url);
 
   const items = await (await fetch(`${origin}/api/news`)).json();
 
-  return rss({
+  const rssString = await getRssString({
     title: 'Komunikaty WWSI (ale lepsze)',
     description: 'Komunikaty - Studencki Panel Informacyjny',
     site: 'https://student.wwsi.edu.pl/info',
     items,
   });
+
+  return new Response(rssString, {
+		headers: {
+			'Content-Type': 'application/xml',
+      'Cache-Control': 'public, s-maxage=1800',
+		},
+	});
 }
